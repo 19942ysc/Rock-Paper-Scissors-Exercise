92a25bc30d0d9e5eb75cd1014de9122435debb56
19942ysc <108676632+19942ysc@users.noreply.github.com>
2022-07-19 18:44:41 -0400

    Update game.py

--- game.py
+++ game.py
@@ -2,7 +2,7 @@
 
 import random
 
-print("Welcome 'Player One' to my Rock, Paper, Scissors Game")
+print("Welcome 'Player One' to My Rock, Paper, Scissors Game")
 
 
 #Processing user inputs
@@ -13,29 +13,29 @@ user_input = user_input.lower()
 #Validating user inputs
 valid_options = ["rock", "paper", "scissors"]
 if user_input in valid_options:
-	print("You chose:", user_input)
+	print("You choose:", user_input)
 
 
 	#Simulating computer selection
-	computer_choice = random.choice(valid_options)
-	print("Computer chose:", computer_choice)
+	computers_choice = random.choice(valid_options)
+	print("Computer chooses:", computers_choice)
 
 
 	#Determining winner
-	if user_input == computer_choice:
+	if user_input == computers_choice:
 		print("TIE")	
 	elif user_input == "rock":
-		if computer_choice == "scissors":
+		if computers_choice == "scissors":
 			print("You Win!")
 		else:
 			print("You Lost.")
 	elif user_input == "paper":
-		if computer_choice == "rock":
+		if computers_choice == "rock":
 			print("You Win!")
 		else:
 			print("You Lost.")
 	elif user_input == "scissors":
-		if computer_choice == "paper":
+		if computers_choice == "paper":
 			print("You Win!")
 		else:
 			print("You Lost.")	
